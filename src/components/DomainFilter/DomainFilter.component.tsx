3ef4b264b2d4cbae28a28940f92237f0a0a65cf2
Benoit <bgrasset@payplug.com>
2022-03-21 12:42:16 +0100

    add initial code

--- src/components/DomainFilter/DomainFilter.component.tsx
+++ src/components/DomainFilter/DomainFilter.component.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { getCountries, getClassifications, getSubClassifications } from '../../utils/utils';
-import '../../app.css';
+import { threadId } from 'worker_threads';
 
 interface State {
   countries: string[],
@@ -13,26 +12,46 @@ interface Props {
 }
 
 class DomainFilter extends React.Component<Props, State> {
-
-  state: State = {
-    countries: [],
-    classifications: [],
-    subClassifications: []
-  };
-
   componentDidMount() {
-    const { domains } = this.props;
+    const { domains } = this.props
+    this.state = {
+      countries: [],
+      classifications: [],
+      subClassifications: []
+    }
 
-    this.setState(prevState => ({
-      ...prevState,
-      countries: getCountries(domains),
-      subClassifications: getSubClassifications(domains),
-      classifications: getClassifications(domains)
-    }))
+    const s: any = {};
+
+    for(let i = 0; i < domains.length; i++) {
+      if (this.state.countries.indexOf(domains[i].substring(0,2)) <= 0) {
+        this.state.countries.push(domains[i].substring(0,2))
+      }
+      this.state.classifications.push(domains[i].substring(3,5));
+      let flag = false;
+      for(let j = 0; j < this.state.subClassifications.length; j++) {
+        if (this.state.subClassifications[j] == domains[i].substring(6)) {
+          flag = true
+          break;
+        }
+      }
+      if (!flag) {
+        this.state.subClassifications.push(domains[i].substring(6));
+      }
+    }
+
+    this.setState({
+      ...this.state,
+      classifications: this.state.classifications.filter((e, i, l) => l.indexOf(e) === i),
+    })
+    this.forceUpdate()
   }
 
   render() {
-    const { countries, classifications, subClassifications } = this.state;
+    const {countries, classifications, subClassifications} = this.state || {
+      countries: [],
+      classifications: [],
+      subClassifications: []
+    };
 
     return (<>
       <select name="countries" multiple>
